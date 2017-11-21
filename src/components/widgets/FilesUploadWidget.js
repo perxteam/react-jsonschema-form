1c0a193a6d48bd02b470b94f9768a3e58d758fbe
Oleynik Dmitry <oleynik@adwz.ru>
2017-11-21 14:19:55 +0300

    https://github.com/perxteam/form-service/issues/75 in progress: update react-uploader

--- src/components/widgets/FilesUploadWidget.js
+++ src/components/widgets/FilesUploadWidget.js
@@ -18,6 +18,7 @@ class FileUploadWidget extends Component {
       <div>
         <Uploader
           onChange={this.handleChange}
+          actualDelete={false}
           {...options}
         />
       </div>
