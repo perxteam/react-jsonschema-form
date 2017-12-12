6799676531810a2da61df25a34b0b5911364120f
Oleynik Dmitry <oleynik@adwz.ru>
2017-12-12 13:13:06 +0300

    https://github.com/perxteam/form-service/issues/146 in progress: done date mask

--- src/components/widgets/DateTimeInputWidget.js
+++ src/components/widgets/DateTimeInputWidget.js
@@ -1,16 +1,17 @@
 import React, {PropTypes} from "react";
 import Datetime from 'react-datetime'
+import InputElement from 'react-input-mask'
 import moment from 'moment'
 import 'moment/locale/ru'
 import 'react-datetime/css/react-datetime.css'
 
 export const formatDateCustom = (format) => (date) => {
-//  console.log('formatDate', date, 'type is', typeof date)
   if (!moment(date, format, true).isValid()) return date
   return date
-  ? moment(new Date(date)).format(format)
-  : undefined
+    ? moment(new Date(date)).format(format)
+    : undefined
 }
+
 export const formatDate = formatDateCustom('DD.MM.YYYY HH:mm')
 
 function isValid(current) {
@@ -18,67 +19,114 @@ function isValid(current) {
   return current.isAfter(yesterday)
 }
 
-function DateTimeInputWidget(props) {
-  const {
-    id,
-    value,
-    readonly,
-    autofocus,
-    onChange,
-    options,  // eslint-disable-line
-    schema,   // eslint-disable-line
-    formContext,  // eslint-disable-line
-    registry, // eslint-disable-line
-    ...inputProps
-  } = props;
+class DateTimeInputWidget extends React.Component {
+  renderInput = () => {
+    const {
+      value,
+      onChange,
+    } = this.props;
 
-//  console.log('value', value , 'is date valid:', moment(value, dateTimeFormat, true).isValid(), 'js format', new Date(value))
-  const { dateTimeWidgetType } = options
-  let dateFormat = false, timeFormat = false, placeholder, format, viewMode = 'days'
-  if (!dateTimeWidgetType || dateTimeWidgetType === 'dateTime') {
-    dateFormat = 'DD.MM.YYYY'
-    timeFormat = 'HH:mm'
-    placeholder =  'ДД.ММ.ГГГГ чч:мм'
-    format = 'DD.MM.YYYY HH:mm'
-  } else if (dateTimeWidgetType === 'date') {
-    dateFormat = 'DD.MM.YYYY'
-    placeholder = 'ДД.ММ.ГГГГ'
-    format = 'DD.MM.YYYY'
-  } else if (dateTimeWidgetType === 'time') {
-    timeFormat = 'HH:mm'
-    placeholder = 'ЧЧ:ММ'
-    format = 'HH:mm'
-    viewMode = 'time'
+    const [day = '', month = '', year = ''] = value ? value.split('.') : []
+    let D
+    if (day.startsWith('3')) {
+      D = '[01]'
+    } else if (day.startsWith('0')) {
+      D = '[1-9]'
+    } else {
+      D = '[0-9]'
+    }
+
+    let M
+    if (month.startsWith('1')) {
+      M = '[0-2]'
+    } else if (month.startsWith('0')) {
+      M = '[1-9]'
+    } else {
+      M = '[0-9]'
+    }
+    return (
+      <InputElement
+        mask='dD.mM.y999'
+        maskChar={null}
+        placeholder='ДД.ММ.ГГГГ'
+        onChange={event => onChange(event.target.value)}
+        value={value || ''}
+        formatChars={{
+          '9': '[0-9]',
+          'd': '[0-3]',
+          'D': D,
+          'm': '[01]',
+          'M': M,
+          'y': '[12]'
+        }}
+      />
+    )
   }
-  const { cssPrefix } = formContext
-  return (
-    <Datetime
-      locale="ru"
-      name={id}
-      className={`${cssPrefix}__date-time-picker`}
-      dateFormat={dateFormat}
-      timeFormat={timeFormat}
-      value={value}
-      viewMode={viewMode}
-      timeConstraints={{
-        minutes: {
-          step: 5,
+
+  render() {
+    const {
+      id,
+      value,
+      readonly,
+      autofocus,
+      onChange,
+      options,  // eslint-disable-line
+      schema,   // eslint-disable-line
+      formContext,  // eslint-disable-line
+      registry, // eslint-disable-line
+      ...inputProps
+    } = this.props;
+
+  //  console.log('value', value , 'is date valid:', moment(value, dateTimeFormat, true).isValid(), 'js format', new Date(value))
+    const { dateTimeWidgetType } = options
+    let dateFormat = false, timeFormat = false, placeholder, format, viewMode = 'days'
+    if (!dateTimeWidgetType || dateTimeWidgetType === 'dateTime') {
+      dateFormat = 'DD.MM.YYYY'
+      timeFormat = 'HH:mm'
+      placeholder =  'ДД.ММ.ГГГГ чч:мм'
+      format = 'DD.MM.YYYY HH:mm'
+    } else if (dateTimeWidgetType === 'date') {
+      dateFormat = 'DD.MM.YYYY'
+      placeholder = 'ДД.ММ.ГГГГ'
+      format = 'DD.MM.YYYY'
+    } else if (dateTimeWidgetType === 'time') {
+      timeFormat = 'HH:mm'
+      placeholder = 'ЧЧ:ММ'
+      format = 'HH:mm'
+      viewMode = 'time'
+    }
+    const { cssPrefix } = formContext
+
+    return (
+      <Datetime
+        locale="ru"
+        name={id}
+        className={`${cssPrefix}__date-time-picker`}
+        dateFormat={dateFormat}
+        timeFormat={timeFormat}
+        value={value}
+        viewMode={viewMode}
+        timeConstraints={{
+          minutes: {
+            step: 5,
+          }
+        }}
+        inputProps={
+          {
+            className: formContext && formContext.preview
+              ? 'ant-input ant-input-lg'
+              : `${cssPrefix}__form-control`,
+            placeholder,
+          }
         }
-      }}
-      inputProps={
-        {
-          className: formContext && formContext.preview
-            ? 'ant-input ant-input-lg'
-            : `${cssPrefix}__form-control`,
-          placeholder,
-        }
-      }
-      isValidDate={isValid}
-      onChange={(value) => {
-        onChange(formatDateCustom(format)(value))
-      }}
-    />
-  )
+        isValidDate={isValid}
+        onChange={(value) => {
+          onChange(formatDateCustom(format)(value))
+        }}
+        renderInput={this.renderInput}
+      />
+    )
+  }
 }
 
 export default DateTimeInputWidget
