e319f5183ab9c5372277a3ab92528a467c1110e7
Oleynik Dmitry <oleynik@adwz.ru>
2018-01-31 19:15:21 +0300

    https://github.com/perxteam/react-jsonschema-form/issues/53 in progress

--- test/DateTimeInputWidget.spec.js
+++ test/DateTimeInputWidget.spec.js
@@ -73,31 +73,36 @@ const utils = {
     component.find('.rdtCounter .rdtBtn').at(3).simulate('mouseDown')
   },
   manualInput(component, value) {
-//    const target = component.find('DateTimeInputWidget InputElement').getDOMNode()
-////    const target = component.find('DateTimeInputWidget InputElement').getDOMNode()
-//    ReactTestUtils.Simulate.keyPress(target, { key: value })
-////    ReactTestUtils.Simulate.change(target, { target: { value } })
-
-//    component.find('DateTimeInputWidget InputElement input').prop('onKeyDown')({target: { value }})
-//    console.log('1', component.find('DateTimeInputWidget InputElement input').props())
     const input = component.find('DateTimeInputWidget InputElement').instance()
     const target = component.find('DateTimeInputWidget InputElement input').getDOMNode()
-//    const value = '3121'
 
-//    target.focus()
-//    ReactTestUtils.Simulate.focus(target);
+    target.focus()
+    ReactTestUtils.Simulate.focus(target);
 
     input.setCursorPos(0);
     target.value = value
     input.setCursorPos(value.length);
-//    input.setCursorPos(1);
-//    console.log('aaa', input.getCursorPos())
 
-//    const target = component.find('DateTimeInputWidget InputElement').getDOMNode()
-//    ReactTestUtils.Simulate.keyDown(target, { key: value })
-//    ReactTestUtils.Simulate.change(target, { target: { value } })
     ReactTestUtils.Simulate.change(target)
-//    console.log('value:', target.value)
+    ReactTestUtils.Simulate.change(target)
+    console.log('value:', target.value)
+  },
+  continueInput(component, value) {
+    const input = component.find('DateTimeInputWidget InputElement').instance()
+    const target = component.find('DateTimeInputWidget InputElement input').getDOMNode()
+    const pos = input.getCursorPos();
+    console.log('position', pos)
+
+//    target.focus()
+//    ReactTestUtils.Simulate.focus(target);
+//
+//    input.setCursorPos(0);
+    target.value = value
+    input.setCursorPos(value.length);
+
+    ReactTestUtils.Simulate.change(target)
+    ReactTestUtils.Simulate.change(target)
+    console.log('value:', target.value)
   },
 }
 
@@ -161,21 +166,17 @@ describe('Testing component "DateTimeInput"', function () {
 
   it('should afford to set datetime by manual input', function () {
     const wrapper = createFormComponent({ schema, uiSchema })
-    const target = wrapper.find('DateTimeInputWidget InputElement input').getDOMNode()
     utils.focus(wrapper)
     utils.manualInput(wrapper, '221120')
     assert.equal(wrapper.state().formData.foo, '22.11.20__ __:__')
-//    console.log('aaa', wrapper.find('DateTimeInputWidget InputElement').getDOMNode().getCursorPos())
-//    utils.manualInput(wrapper, '221')
-//    console.log('value', target.value)
-//    utils.manualInput(wrapper, '221120121123')
-//    assert.equal(wrapper.state().formData.foo, '22.11.2012 11:23')
   })
 
-  xit('should prevent from incorrect datetime input', function () {
+  it('should prevent from incorrect datetime input', function () {
     const wrapper = createFormComponent({ schema, uiSchema })
     utils.focus(wrapper)
-    utils.manualInput(wrapper, '40')
-    assert.equal(wrapper.state().formData.foo, '__.__.____ __:__')
+    utils.manualInput(wrapper, '428352120')
+    assert.equal(wrapper.state().formData.foo, '28.12.____ __:__')
+    utils.continueInput(wrapper, '11')
+    console.log('state', wrapper.state().formData.foo)
   })
 })
